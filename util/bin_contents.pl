b5f2df7366b0f27143cfd75c8d8f5348e63198cd
Eric David Moyer <eric_moyer@yahoo.com>
2011-09-28 13:48:59 -0400

    Enabled calculation of number of peaks in a bin as well as number of metabolites

--- util/bin_contents.pl
+++ util/bin_contents.pl
@@ -3,14 +3,30 @@ use strict;
 use warnings;
 
 ##################
-# Usage: metabolites_per_bin.pl bin_width < peak_ppms
+# Usage: bin_contents.pl bin_width [metab*| peak*] < peak_ppms
 #
 # Reads a list of peak ppms (the output of extract_peak_ppms) from
-# stdin and counts the number of metabolites falling in each bin if
-# the bins are bin_width wide.  It prints the results to stdout.
+# stdin and counts the contents of each bin if the bins are bin_width
+# wide.  If the second argument matches metab*, it counts the number
+# of metabolites with a peak in that bin.  If the second argument
+# matches peak*, it counts the number of peaks lying in that bin.  It
+# prints the results to stdout.
 
 my $bin_width = shift;
-my $verbose = @ARGV != 0;
+my $second_arg = shift;
+
+my $to_count;
+sub COUNT_METAB(){ 0 }
+sub COUNT_PEAK(){ 1 }
+if ($second_arg =~ /^metab/i){
+    $to_count = COUNT_METAB;
+}elsif ($second_arg =~ /^peak/i){
+    $to_count = COUNT_PEAK;
+}else{
+    print STDERR "The second argument must start with metab or with peak\n";
+    exit(-1);
+}
+
 my $max_ppm; #Max ppm of any peak of any metabolite
 
 #Read in the metabolites
@@ -32,7 +48,11 @@ my $spectrum_start = -1; #Lower bound of the first bin
 my $bin_num = 0; #Number of the current bin
 my $bin_lb = $spectrum_start+$bin_num*$bin_width; #Bin lower bound
 
-print "Bin Lower\tBin Upper\tNum Metabolites\n"; # Print header
+if($to_count == COUNT_METAB){
+    print "Bin Lower\tBin Upper\tNum Metabolites\n"; # Print header
+}else{
+    print "Bin Lower\tBin Upper\tNum Peaks\n"; # Print header
+}
 while($bin_lb <= $max_ppm){
     #Count the metabolites in the current bin
     my $bin_ub = $bin_lb + $bin_width;
@@ -41,7 +61,9 @@ while($bin_lb <= $max_ppm){
 	foreach my $ppm (@$ppms){
 	    if($bin_lb <= $ppm && $ppm < $bin_ub){
 		++$count;
-		next METABOLITE;
+		if($to_count == COUNT_METAB){
+		    next METABOLITE;
+		}
 	    }
 	}
     }
